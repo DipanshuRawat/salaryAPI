3bb5384276a7e7624c0414213ee4bc88d6f7ad68
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-21 21:40:27 +0530

    Update postFailure.groovy

--- vars/postFailure.groovy
+++ vars/postFailure.groovy
@@ -1,25 +1,11 @@
-def call(String slackChannel, String emailRecipients, String jobName, String buildNumber, String buildUrl) {
-    slackSend(channel: slackChannel, color: 'danger', username: 'Jenkins CI', message: "Build Failed: Job Name: ${jobName}, Build No: ${buildNumber}, Build URL: ${buildUrl}")
-
+def call(String emailRecipients, String jobName, String buildNumber, String buildUrl) {
     emailext(
-        attachmentsPattern: "target/dependency-check-report.html, unit-test-report.txt, zap_report.html",
+        subject: "FAILURE: Job '${jobName}' (${buildNumber})",
         body: """
-            Hello,
-
-            The Jenkins pipeline *${jobName}* has failed on *Build #${buildNumber}*.
-
-            *Job Details:*
-            - *Job Name:* ${jobName}
-            - *Build Number:* ${buildNumber}
-            - *Build URL:* ${buildUrl}
-
-            Please review logs and reports for more details.
-
-            Regards,  
-            Jenkins CI
-            Zero Downtime Crew
+            Job: ${jobName}
+            Build Number: ${buildNumber}
+            Build URL: ${buildUrl}
         """,
-        subject: "Jenkins Pipeline FAILED: ${jobName} #${buildNumber}",
         to: emailRecipients
     )
 }
