35e97f36234ec2f87d0670e680c42ec2b86ee91f
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-22 00:28:13 +0530

    Update dependencyScanning.groovy

--- vars/dependencyScanning.groovy
+++ vars/dependencyScanning.groovy
@@ -1,6 +1,6 @@
 def call() {
     // Increase timeout to 1 hour
     timeout(time: 1, unit: 'HOURS') {
-        sh 'mvn org.owasp:dependency-check-maven:check -Dformat=HTML -DoutputDirectory=target'
+        sh 'mvn org.owasp:dependency-check-maven:check -Dformat=HTML -DoutputDirectory=target || true' 
     }
 }
