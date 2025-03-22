4208c8d954c6c3a5ad6658949324720c93a7b21a
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-22 12:04:55 +0530

    Update dependencyScanning.groovy

--- vars/dependencyScanning.groovy
+++ vars/dependencyScanning.groovy
@@ -1,6 +1,6 @@
 def call() {
     // Increase timeout to 1 hour
     timeout(time: 1, unit: 'HOURS') {
-         sh './dependency-check/bin/dependency-check.sh --project "." --scan . --format HTML --out security-reports'
+         sh './dependency-check/dependency-check/bin/dependency-check.sh --project "." --scan . --format HTML --out security-reports'
     }
 }
