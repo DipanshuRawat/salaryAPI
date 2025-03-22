e74e4d4492686254ad863382cee76f808f5e19fc
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-22 12:59:33 +0530

    Update bugAnalysis.groovy

--- vars/bugAnalysis.groovy
+++ vars/bugAnalysis.groovy
@@ -5,7 +5,7 @@ def call(String projectKeyName, String sonarToken) {
             mvn clean verify sonar:sonar \
               -Dsonar.projectKey="${projectKey_name}" \
               -Dsonar.host.url=http://3.80.43.200:9000 \
-              -Dsonar.login="${SONARQUBE_TOKEN}"
+              -Dsonar.login="${SONARQUBE_TOKEN} || true"
             '''
         }
     }
