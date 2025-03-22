0b42ffcc4a64d21afbd2d4a7d8fdb6f58df5410b
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-22 13:12:06 +0530

    Update bugAnalysis.groovy

--- vars/bugAnalysis.groovy
+++ vars/bugAnalysis.groovy
@@ -1,12 +1,12 @@
-def call(String projectKeyName, String sonarToken) {
+def call(String projectKeyName, String sonarCredentialsId) {
     withSonarQubeEnv('sonar') {
-        withCredentials([string(credentialsId: sonarToken, variable: 'soanr_token')]) {
-            sh '''
+        withCredentials([string(credentialsId: sonarCredentialsId, variable: 'SONAR_TOKEN')]) {
+            sh """
             mvn clean verify sonar:sonar \
-              -Dsonar.projectKey=JAVA-Bug \
+              -Dsonar.projectKey=${projectKeyName} \
               -Dsonar.host.url=http://3.80.43.200:9000 \
-              -Dsonar.login=${sonar_token}
-            '''
+              -Dsonar.login=${SONAR_TOKEN}
+            """
         }
     }
 }
