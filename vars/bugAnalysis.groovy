2c4a9e9d1e996737774f28363775e69abc8b9057
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-22 14:33:32 +0530

    Update bugAnalysis.groovy

--- vars/bugAnalysis.groovy
+++ vars/bugAnalysis.groovy
@@ -1,12 +1,3 @@
-def call(String projectKey, String credentialsId ) {
-    withSonarQubeEnv('sonar') { 
-        withCredentials([string(credentialsId: credentialsId, variable: 'SONARQUBE_TOKEN')]) {
-            sh """
-            mvn clean verify sonar:sonar \
-            -Dsonar.projectKey=${projectKey} \
-            -Dsonar.login=${SONARQUBE_TOKEN} \
-            -Dsonar.projectName='${projectKey}' \
-            """
-        }
-    }
+def call(String projectKey, String credentialsId) {
+    sh 'mvn spotbugs:check > spotbugs-check-output.txt'
 }
